46289359a6afb5ba657142527e5f17cf77218cdc
yetangcun <302982814@qq.com>
2026-01-29 11:31:58 +0800

    ....

--- lucky.front/vite.config.ts
+++ lucky.front/vite.config.ts
@@ -6,13 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 6116,
   },
 })
